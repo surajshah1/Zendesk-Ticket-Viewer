a1b99f97e85d864299808cee5daba62926c6fe95
Suraj Shah <surajshah535@gmail.com>
2020-11-15 00:27:16 -0800

    added post

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 var routes = require("./routes")
+
 
 var app = express();
 
@@ -13,6 +15,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(routes);
+
+
 
 app.listen(app.get("port"),function(){
     console.log("Server started on http://localhost:" + app.get("port"));
--- routes.js
+++ routes.js
@@ -1,21 +1,29 @@
 var express = require("express");
-
 var router = express.Router();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 router.get("/", function(req, res){
   console.log("Log: You are on the  --HOME--  page")
+
   res.render("index");
 });
 
 router.get("/about", function(req, res){
   console.log("Log: You are on the  *ABOUT*   page")
+
   res.render("about");
 });
 
 router.get("/login", function(req, res){
   console.log("Log: You are on the  $LOGIN$   page")
+
   res.render("login");
 });
 
+router.post('/login', urlencodedParser, function (req, res) {
+  //console.log(req.body)
+  res.send('the link is: ' + req.body.subdomain + ', email is: ' + req.body.email + ', password is: ' + req.body.password)
+})
 
 module.exports = router;
