7da9ca7a5f7f2d9bd0b094f27da96c16b9b452ab
Suraj Shah <surajshah535@gmail.com>
2020-11-14 21:10:06 -0800

    improved console logs

--- routes.js
+++ routes.js
@@ -3,17 +3,17 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res){
-  console.log("Log: You are on the start page")
+  console.log("Log: You are on the  --HOME--  page")
   res.render("index");
 });
 
 router.get("/about", function(req, res){
-  console.log("Log: You are on the about page")
+  console.log("Log: You are on the  *ABOUT*   page")
   res.render("about");
 });
 
 router.get("/login", function(req, res){
-  console.log("Log: You are on the login page")
+  console.log("Log: You are on the  $LOGIN$   page")
   res.render("login");
 });
 
